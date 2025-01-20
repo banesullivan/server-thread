f8e7d4f8fde3b2b146ea9ecaff6525c5295b682a
vincentsarago <vincent.sarago@gmail.com>
2025-01-20 18:37:31 +0100

    update python versions and switch to pyproject.toml

--- server_thread/__init__.py
+++ server_thread/__init__.py
@@ -1,4 +1,6 @@
 # flake8: noqa: F401
-from server_thread._version import __version__
 from server_thread.report import Report
 from server_thread.server import ServerDownError, ServerManager, ServerThread, launch_server
+
+
+__version__ = "0.2.0"
